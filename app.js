d1ed3aaa7da6cfb855fcaeb92f1ab4686521944e
woohyein <whyein@kaist.ac.kr>
2021-02-15 17:28:58 +0900

    로그인

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const dotenv = require('dotenv');
+
 const userRouter = require('./routes/user');
 const db = require('./models');
+const passportConfig = require('./passport');
+
 const app = express();
 
+dotenv.config();
 db.sequelize.sync()
   .then(() => {
     console.log('db 연결 성공!');
   })
   .catch(console.error);
 
+  passportConfig();
+
   app.use(cors({
     origin: '*',
   }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser( process.env.COOKIE_SECRET));
+app.use(session({
+  saveUninitialized: false,
+  resave: false,
+  secret: process.env.COOKIE_SECRET,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('hello express');
